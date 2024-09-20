160b7978b5a75124fc5db492e802ac51eb7af709
AntoineCoignac <acoignac35@gmail.com>
2024-09-20 21:11:51 +0200

    skeleton cards

--- app/components/CtaCard.jsx
+++ app/components/CtaCard.jsx
@@ -1,13 +1,20 @@
 import Link from "next/link";
 import "./CtaCard.scss";
+import { useRouter } from "next/navigation";
 
 export default function CtaCard({title="", button="", to="/", style=""}) {
+    const router = useRouter();
+
+    const handleClick = () => {
+        router.push(to);
+    }
+
     return (
-        <Link href={to} className={`cta-card ${style}`}>
+        <div onClick={handleClick} className={`cta-card ${style}`}>
             <div className="cta-card-wrapper">
-                <span className="text-regular white-text">{title}</span>
-                <span className="no-click-btn">{button}</span>
+                <span className="text-regular text-white" id="title">{title}</span>
+                <span id="no-click-btn">{button}</span>
             </div>
-        </Link>
+        </div>
     )
 }
--- app/components/Ctas.jsx
+++ app/components/Ctas.jsx
@@ -1,22 +1,47 @@
 'use client'
 
-import {Splide, SplideTrack, SplideSlide} from "@splidejs/react-splide";
+import {Splide, SplideSlide} from "@splidejs/react-splide";
 import "@splidejs/react-splide/css";
 import CtaCard from "./CtaCard";
 import "./Ctas.scss";
+import { useState, useEffect } from "react";
 
 export default function Ctas() {
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setIsLoading(false);
+        }, 400);
+
+        return () => clearTimeout(timer);
+    }, []);
+
     return (
-        <Splide options={{arrows: false, pagination: true, width: "100%", gap: "16px"}}>
-                <SplideSlide>
-                    <CtaCard title="Crée un résumé de cours avec l’IA" button="Commencer à enregistrer" to="/record" style="magic" />
-                </SplideSlide>
-                <SplideSlide>
-                    <CtaCard title="Mets 5 étoiles à Swite sur le store" button="Donner mon avis" to="/" style="primary" />
-                </SplideSlide>
-                <SplideSlide>
-                    <CtaCard title="Partage l’application avec tes amis" button="Copier le lien" to="/" style="primary" />
-                </SplideSlide>
+        isLoading ? 
+        <div className="blank-ctas">
+            <div className="blank-ctas-track">
+                <div className="blank-ctas-slide"></div>
+                <div className="blank-ctas-slide"></div>
+                <div className="blank-ctas-slide"></div>
+            </div>
+            <div className="blank-ctas-pagination">
+                <div className="blank-ctas-pagination-page"></div>
+                <div className="blank-ctas-pagination-page"></div>
+                <div className="blank-ctas-pagination-page"></div>
+            </div>
+        </div> 
+        :
+        <Splide className="ctas" options={{arrows: false, pagination: true, width: "100%", gap: "16px"}}>
+            <SplideSlide>
+                <CtaCard title="Crée un résumé de cours avec l’IA" button="Commencer à enregistrer" to="/record" style="magic" />
+            </SplideSlide>
+            <SplideSlide>
+                <CtaCard title="Mets 5 étoiles à Swite sur le store" button="Donner mon avis" to="/" style="primary" />
+            </SplideSlide>
+            <SplideSlide>
+                <CtaCard title="Partage l’application avec tes amis" button="Copier le lien" to="/" style="primary" />
+            </SplideSlide>
         </Splide>
     );
 }
