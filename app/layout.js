5dd31a22668faa884b2c7addcb793f500a5ff92c
AntoineCoignac <acoignac35@gmail.com>
2024-09-21 15:00:31 +0200

    metadata

--- app/layout.js
+++ app/layout.js
@@ -3,12 +3,12 @@ import NextAuthProvider from "./Providers";
 
 export const metadata = {
   title: "Swite - Apprenez sans effort",
-  description: "Swite est une application qui vous permet d'apprendre sans effort.",
+  description: "Swite est l'application qui vous permet d'apprendre sans effort, grâce au speech to text qui enregistre vos cours. L'app permet également d'apprendre en écoutant grâce au text to speech.",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="fr">
       <body>
         <NextAuthProvider>
           {children}
